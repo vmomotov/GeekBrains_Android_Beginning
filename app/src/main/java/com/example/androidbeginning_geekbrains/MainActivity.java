754d70847ba680686b266a5d048cccf4a140d41c
vmomotov <v.s.momotov@gmail.com>
2022-06-29 23:46:18 +0300

    initial

---
+++ app/src/main/java/com/example/androidbeginning_geekbrains/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidbeginning_geekbrains;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
